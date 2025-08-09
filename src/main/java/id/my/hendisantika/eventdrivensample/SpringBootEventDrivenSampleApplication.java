0f3d7919426416700f55b1c818b74bd587f97860
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 06:25:00 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/eventdrivensample/SpringBootEventDrivenSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.eventdrivensample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEventDrivenSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEventDrivenSampleApplication.class, args);
+    }
+
+}
