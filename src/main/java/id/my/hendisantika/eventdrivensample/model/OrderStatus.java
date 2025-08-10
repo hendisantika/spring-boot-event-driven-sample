f832c668b42fef79f7145b11a58cd3546f0e3fec
hendisantika <hendisantika@yahoo.co.id>
2025-08-11 05:17:18 +0700

    feat: Add OrderStatus Enum Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/eventdrivensample/model/OrderStatus.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.eventdrivensample.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-driven-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 06.25
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderStatus {
+    CREATED,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
