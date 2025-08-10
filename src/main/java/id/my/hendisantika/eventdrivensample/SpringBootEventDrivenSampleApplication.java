47c87600be811be6ba79f3e11bbbe5dccc46bf40
hendisantika <hendisantika@yahoo.co.id>
2025-08-11 05:13:39 +0700

    feat: Add required libs🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/eventdrivensample/SpringBootEventDrivenSampleApplication.java
+++ src/main/java/id/my/hendisantika/eventdrivensample/SpringBootEventDrivenSampleApplication.java
@@ -3,6 +3,18 @@ package id.my.hendisantika.eventdrivensample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-driven-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 06.15
+ * To change this template use File | Settings | File Templates.
+ */
+
 @SpringBootApplication
 public class SpringBootEventDrivenSampleApplication {
 
--- src/test/java/id/my/hendisantika/eventdrivensample/SpringBootEventDrivenSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/eventdrivensample/SpringBootEventDrivenSampleApplicationTests.java
@@ -3,6 +3,18 @@ package id.my.hendisantika.eventdrivensample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-driven-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 06.50
+ * To change this template use File | Settings | File Templates.
+ */
+
 @SpringBootTest
 class SpringBootEventDrivenSampleApplicationTests {
 
