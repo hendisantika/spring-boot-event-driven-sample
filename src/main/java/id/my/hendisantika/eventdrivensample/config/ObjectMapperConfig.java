def201151f9a6a902e80b5b90ed20eac57156cc2
hendisantika <hendisantika@yahoo.co.id>
2025-08-11 05:15:39 +0700

    feat: Add ObjectMapperConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/eventdrivensample/config/ObjectMapperConfig.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.eventdrivensample.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-driven-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 06.48
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
